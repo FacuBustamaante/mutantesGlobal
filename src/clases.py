17343898931d27dc5e60c902c644e274bbe47172
facundo <facundombustamante@gmail.com>
2024-11-08 23:39:46 -0300

    Se terminó la superclase mutador (Falta evaluación)

--- src/clases.py
+++ src/clases.py
@@ -1,6 +1,36 @@
+import re
+
 class Mutador():
-    def __init__(self):
-        print(self)
+    adn_mutantre = ["A", "T", "C", "G"]
+    adn_tamanio = ""
+    mutacion = ""
+    
+    #Ingreso de la base que se repetirá en la mutación.
+    base_nitrogenada = input("""
+                             Elija la base nitrogenada que desea insertar: 
+                             Adenina (A), Timina (T), Citosina (C) y Guanina (G).
+                             >""").upper()
+    
+    #Evaluación de que la base ingresada sea correcta.
+    while True:
+        if not re.match("^([ATGC]*)$",base_nitrogenada):
+            print("""El dato ingresado es incorrecto, vuelva a intentarlo""")
+            base_nitrogenada = input("""
+                Elija la base nitrogenada que desea insertar: 
+                Adenina (A), Timina (T), Citosina (C) y Guanina (G).
+                >""").upper()
+        else:
+            print("Mutación ingresada correctamente")
+            break
+            
+    #Método constructor
+    def __init__(self, base_nitrogenada, adn_tamanio, mutacion):
+        self.base_nitrogenada = base_nitrogenada
+        self.adn_tamanio = adn_tamanio
+        self.mutacion = mutacion
+    
+    def crear_mutante():
+        pass
         
 class Detector():
     nombre = "facundo"
