c743bf1f4aa864786abac912c0bb531555db90e7
LayonelMontivero <montiverolionel@gmail.com>
2024-11-14 11:55:58 -0300

    Update clases.py
    Aun falta agregarle cosas a la clase, más que nada comentarios y la segunda parte de la detección en diagonal,

--- src/clases.py
+++ src/clases.py
@@ -32,14 +32,37 @@ class Mutador():
     def crear_mutante():
         pass
         
-class Detector():
-    nombre = "facundo"
+class Detector:
+    #Falta agregar una cosa más de la funcion diagona, agregar comentarios explicando la logica y verificar todo en su conjunto
+    def __init__(self, matriz):
+        self.matriz = [list(fila) for fila in matriz]
+
+    def detectar_mutantes(self):
+        return (self.Mut_horizontal() or self.mut_vertical() or self.mut_diagonal())
+                
+    def Mut_horizontal(self):
+        for fila in self.matriz:
+            for i in range(len(fila) - 3):  
+                if fila[i] == fila[i+1] == fila[i+2] == fila[i+3]:
+                    return "Mutacion horizontal"
+        return None
     
-    def __init__(self):
-        print("Detector")
+    def mut_vertical(self):
+        for col in range(len(self.matriz[0])):  
+            for fil in range(len(self.matriz) - 3):
+                if (self.matriz[fil][col] == self.matriz[fil+1][col] ==
+                    self.matriz[fil+2][col] == self.matriz[fil+3][col]):
+                    return "Mutacion vertical"
+        return None
+
+    def mut_diagonal(self):
+        for fil in range(len(self.matriz)-3):  
+            for col in range(len(self.matriz[0]) - 3):
+                if (self.matriz[fil][col] == self.matriz[fil+1][col+1] ==
+                    self.matriz[fil+2][col+2] == self.matriz[fil+3][col+3]):
+                    return "Mutacion Diagonal"
+        return None
     
-    def __str__(self):
-        return f"nombre: {self.nombre}"
 
 class Radiacion(Mutador):
     pass
