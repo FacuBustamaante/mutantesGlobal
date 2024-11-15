151de35b4c0253b90cad77de596ea968ef94f952
facundo <facundombustamante@gmail.com>
2024-11-14 21:46:39 -0300

    Se limpió el código del archivo ejecutable.py

--- src/ejecutable.py
+++ src/ejecutable.py
@@ -6,45 +6,40 @@ Se que la detector tiene que dar un booleano pero lo hice de esta forma así en 
 se si lo esta detectando bien, cuando me asegure de eso lo cambio a booleano para que solo de V o F
 """
 matriz= []
-#La funcion pide al usuario que ingrese el ADN mediante un bucle, de esta forma lo puede hacer fila por fila
+
+#Input de matriz 
 def ingresarADN():
  for i in range(6):
     while True:
-        #Le pide al usuario que ingrese la fila correspondiente del ADN, elimina espacios en blanco
-        #y hace todo a mayusculas
         fila = input(f"Ingrese la fila {i + 1} de la matriz (6 caracteres)> ").strip().upper()
-        #Comprueba que lo ingresado sea de 6 caracteres y sean los caracteres correctos
         if len(fila) == 6 and all(base in 'ATCG' for base in fila):
-            #Agrega a la matriz cada fila ingresado por el usuario como una lista
-            #De esta forma la matriz es una lista de cada fila que contiene una lista con los caracteres
             matriz.append(list(fila))  
             break 
         else:
             print("--ERROR--")
 
-#Es el menu en si, se le explica al usuario como, que ingresar y se le da un ejemplo
 def menu():
     print("""
-    Ingrese un ADN que contenga las 4 bases nitrogenadas (una fila a la vez)
-    (Adenina (A), Timina (T), Citosina (C) y Guanina (G))
-    --EJEMPLO--
-    TTTTCA, GATTCA, CAACAT, GAGCTA, ATTGCG, CTGTTC
-    --EJEMPLO
-    """)
-    #se llama a la funcion para que ingrese el ADN
+        Ingrese un ADN que contenga las 4 bases nitrogenadas (una fila a la vez)
+        (Adenina (A), Timina (T), Citosina (C) y Guanina (G))
+        --EJEMPLO--
+        TTTTCA, GATTCA, CAACAT, GAGCTA, ATTGCG, CTGTTC
+        --EJEMPLO
+        """)
     ingresarADN()
-    #Una vez terminado de ingresar el ADN se le muestra un menu para que indique que desee hacer con ese ADN
+    
+    #Menu
     while True: 
         print("""
-    --MENU--
-    1: Detectar si hay mutaciones en el ADN
-    2:Mutar ADN
-    3:Sanar ADN
-    4:Mostrar ADN ingresado
-    --MENU--
-    """)
-        OpcionU = int(input("Seleccione una opcion: "))
-        #Se usa match  para que sea más facil de leer y se llama a la clase correspondiente en base a la opcion
+            --MENU--
+            1. Detectar si hay mutaciones en el ADN
+            2. Mutar ADN
+            3. Sanar ADN
+            4. Mostrar ADN ingresado
+            --MENU--
+            """)
+        OpcionU = int(input("Seleccione una opción: "))
+        
         match OpcionU:
             case 1: 
                 print("--Se llama a la funcion para detectar mutaciones--")
