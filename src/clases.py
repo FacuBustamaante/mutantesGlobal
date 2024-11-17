612331ae05774ddf39a930796d0499de8eed2110
facundo <facundombustamante@gmail.com>
2024-11-17 14:49:30 -0300

    Se arregló error en input y actualizó el menú de opciones

--- src/clases.py
+++ src/clases.py
@@ -2,34 +2,13 @@ import re
 
 class Mutador():
     adn_mutantre = ["A", "T", "C", "G"]
-    adn_tamanio = ""
-    mutacion = ""
-    
-    #Ingreso de la base que se repetirá en la mutación.
-    base_nitrogenada = input("""
-                             Elija la base nitrogenada que desea insertar: 
-                             Adenina (A), Timina (T), Citosina (C) y Guanina (G).
-                             >""").upper()
-    
-    #Evaluación de que la base ingresada sea correcta.
-    while True:
-        if not re.match("^([ATGC]*)$",base_nitrogenada):
-            print("""El dato ingresado es incorrecto, vuelva a intentarlo""")
-            base_nitrogenada = input("""
-                Elija la base nitrogenada que desea insertar: 
-                Adenina (A), Timina (T), Citosina (C) y Guanina (G).
-                >""").upper()
-        else:
-            print("Mutación ingresada correctamente")
-            break
+    base_nitrogenada = " "
             
     #Método constructor
-    def __init__(self, base_nitrogenada, adn_tamanio, mutacion):
+    def __init__(self, base_nitrogenada):
         self.base_nitrogenada = base_nitrogenada
-        self.adn_tamanio = adn_tamanio
-        self.mutacion = mutacion
     
-    def crear_mutante():
+    def crear_mutante(self, matriz):
         pass
         
 class Detector:
--- src/ejecutable.py
+++ src/ejecutable.py
@@ -1,10 +1,4 @@
-from clases import  Detector, Mutador, Radiacion, Virus, Sanador
-
-"""
-Hay cosas por modificar a medida que se vayan haciendo el resto de clases
-Se que la detector tiene que dar un booleano pero lo hice de esta forma así en un futuro
-se si lo esta detectando bien, cuando me asegure de eso lo cambio a booleano para que solo de V o F
-"""
+from clases import *
 matriz= []
 
 #Input de matriz 
@@ -29,6 +23,9 @@ def menu():
     ingresarADN()
     
     #Menu
+    print(f"La matriz ingresada es: ")
+    for x in matriz:
+        print(x)
     while True: 
         print("""
             --MENU--
@@ -38,25 +35,44 @@ def menu():
             4. Mostrar ADN ingresado
             --MENU--
             """)
-        OpcionU = int(input("Seleccione una opción: "))
+        opcionU = int(input("Seleccione una opción: "))
         
-        match OpcionU:
+        match opcionU:
             case 1: 
                 print("--Se llama a la funcion para detectar mutaciones--")
                 ADNU=Detector(matriz)
                 TipoM = ADNU.detectar_mutantes()
                 if TipoM:
                     print(f"Se detectó una {TipoM} en el ADN.")
+                    #print(f" ".join(matriz[0]), "\n", " ".join(matriz[1]))
                 else:
                     print("No se detectaron mutaciones en el ADN.")
                 
             case 2: 
-                print("--Se llama a la funcion para mutar el ADN--")
-                
+                    mutacion = input("""
+                                Elija la base nitrogenada que desea insertar: 
+                                Adenina (A), Timina (T), Citosina (C) y Guanina (G).
+                                >""").upper()
+    
+                    #Evaluación de que la base ingresada sea correcta.
+                    while True:
+                        if not re.match("^([ATGC]*)$",mutacion):
+                            print("""El dato ingresado es incorrecto, vuelva a intentarlo""")
+                            mutacion = input("""
+                                Elija la base nitrogenada que desea insertar: 
+                                Adenina (A), Timina (T), Citosina (C) y Guanina (G).
+                                >""").upper()
+                        else:
+                            base_nitrogenada = mutacion * 4
+                            print(f"La base nitrogenada ingresada es {base_nitrogenada}.")
+                            print("Mutación ingresada correctamente")
+                            break
             case 3: 
+                ADNU = Sanador(matriz)
                 print("--Se llama a la funcion para sanar mutaciones--")
                
             case 4: 
                 print("--Se imprime el ADN ingresado--")
                 print(matriz)
 menu()
+
